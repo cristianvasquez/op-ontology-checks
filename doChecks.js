14945c95741e82302877f4bd80b7afd9e4d78eab
Cristian <cristianvasquez@gmail.com>
2023-12-19 15:36:37 +0100

    check authority

--- doChecks.js
+++ doChecks.js
@@ -4,28 +4,40 @@ import { glob } from 'glob'
 import { prettyPrint } from './src/support.js'
 import rdf from 'rdf-ext'
 
+function noAuthority ({ dataset }) {
+  const forbidden = 'http://publications.europa.eu/resource/authority'
+  return [...dataset].filter(quad => quad.subject.value.startsWith(forbidden))
+}
+
+function doReport ({ errors }) {
+  console.log(`Errors: ${errors.length}`)
+  for (const current of errors) {
+    console.log(JSON.stringify(current, null, 2))
+  }
+}
+
 async function doChecks ({ targetDir }) {
   const turtles = await glob(`${targetDir}/**/*.ttl`, {
     stat: true, nodir: true,
   })
 
-  const parsingErrors = []
+  const errors = []
   for (const path of turtles) {
     const fileStream = fs.createReadStream(path, 'utf-8')
+    const dataset = rdf.dataset()
     try {
-      const dataset = rdf.dataset()
       await dataset.import(formats.parsers.import('text/turtle', fileStream))
     } catch (e) {
-      parsingErrors.push({
-        path, e,
+      errors.push({
+        type: 'Parsing error', path, e,
       })
+    }
+    for (const quad of noAuthority({ dataset })) {
+      errors.push({ type: 'no http://publications.europa.eu/resource/authority namespace allowed', path, offendingQuad:quad })
     }
   }
 
-  console.log(`parsing errors: ${parsingErrors.length}`)
-  for (const current of parsingErrors) {
-    console.log(JSON.stringify(current, null, 2))
-  }
+  doReport({ errors })
 }
 
 const targetDir = 'latest'
